7e3fd27f5e152c9ca65e761c64f0870375922629
Juan Martin Verrelli <juanm.verrelli@gmail.com>
2014-05-16 14:12:41 -0300

    Subo la base modificada

--- datos/kiosco.sql
+++ datos/kiosco.sql
@@ -1,10 +1,10 @@
 CREATE DATABASE  IF NOT EXISTS `Kiosco` /*!40100 DEFAULT CHARACTER SET latin1 */;
 USE `Kiosco`;
--- MySQL dump 10.13  Distrib 5.5.35, for debian-linux-gnu (x86_64)
+-- MySQL dump 10.13  Distrib 5.5.37, for debian-linux-gnu (i686)
 --
 -- Host: localhost    Database: Kiosco
 -- ------------------------------------------------------
--- Server version	5.5.35-0+wheezy1
+-- Server version	5.5.37-0+wheezy1
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -45,6 +45,34 @@ INSERT INTO `proveedor` VALUES (1,1534466777,'Guaha','Kiosco',1),(2,474945454,'c
 UNLOCK TABLES;
 
 --
+-- Table structure for table `usuario`
+--
+
+DROP TABLE IF EXISTS `usuario`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `usuario` (
+  `idusuario` int(11) NOT NULL AUTO_INCREMENT,
+  `detalle` varchar(45) DEFAULT NULL,
+  `idturno` int(11) DEFAULT NULL,
+  `contrasena` varchar(60) DEFAULT NULL,
+  `habilitado` tinyint(1) DEFAULT NULL,
+  `idperfil` int(11) DEFAULT NULL,
+  PRIMARY KEY (`idusuario`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `usuario`
+--
+
+LOCK TABLES `usuario` WRITE;
+/*!40000 ALTER TABLE `usuario` DISABLE KEYS */;
+INSERT INTO `usuario` VALUES (1,'tutox',1,'tutox',1,NULL),(2,'tuto',1,'bae5be47227f4f1372e758edc221baf3',0,NULL),(3,'juan',2,'fe0ec43cc4200e777aa2190ace58e7b4',1,NULL),(4,'juan2',2,'a7144ee699113faf18538ab0a2614d19',1,NULL);
+/*!40000 ALTER TABLE `usuario` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `producto`
 --
 
@@ -60,9 +88,9 @@ CREATE TABLE `producto` (
   `datetime` datetime DEFAULT NULL,
   `descripcion` varchar(45) DEFAULT NULL,
   `habilitado` tinyint(1) DEFAULT '1',
-  `rubro` int(11) DEFAULT NULL,
+  `idrubro` int(11) DEFAULT NULL,
   PRIMARY KEY (`idproducto`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=29 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -71,7 +99,7 @@ CREATE TABLE `producto` (
 
 LOCK TABLES `producto` WRITE;
 /*!40000 ALTER TABLE `producto` DISABLE KEYS */;
-INSERT INTO `producto` VALUES (0,2147483647,10,3.50000,4.00000,'0000-00-00 00:00:00','Galletitas Oreo',1,1),(1,1519948948,5,5.70000,10.00000,'0000-00-00 00:00:00','Snacks',0,6),(2,1801,10,5.80000,6.50000,'2014-04-17 12:26:06','coca cola',1,6);
+INSERT INTO `producto` VALUES (0,2147483647,10,3.50000,4.00000,'0000-00-00 00:00:00','Galletitas Oreo',0,1),(1,1519948948,5,5.70000,10.00000,'0000-00-00 00:00:00','Snacks',0,6),(2,1801,10,5.80000,6.50000,'2014-04-17 12:26:06','coca cola',0,6),(3,3,10,5.00000,6.00000,'2014-05-14 12:36:53','Cavernicola',1,1),(4,3,10,5.00000,6.00000,'2014-05-14 12:39:43','Cavernicola',1,1),(5,3,10,5.00000,6.00000,'2014-05-14 12:43:06','Hue',1,1),(6,3,10,5.00000,6.00000,'2014-05-14 12:45:30','Hue',1,1),(7,3,10,5.00000,6.00000,'2014-05-14 12:47:59','Hue2',1,1),(8,3,10,5.00000,6.00000,'2014-05-14 12:50:08','Hue3',1,1),(9,3,10,5.00000,6.00000,'2014-05-14 13:25:22','Hue4',1,1),(10,3,10,5.00000,6.00000,'2014-05-14 13:55:18','Hue5',1,1),(11,3,10,5.00000,6.00000,'2014-05-14 14:03:07','Hue7',1,1),(12,3,10,5.00000,6.00000,'2014-05-14 14:05:29','Hue8',1,1),(13,3,10,5.00000,6.00000,'2014-05-14 14:07:40','Hue9',1,1),(14,12,112,12.00000,12.00000,'2014-05-14 14:17:17','ASD',1,12),(15,12,112,12.00000,12.00000,'2014-05-14 14:17:58','ASD',1,12),(16,12,112,12.00000,12.00000,'2014-05-14 14:18:40','ASD',1,12),(17,12,112,12.00000,12.00000,'2014-05-14 14:19:21','ASD',1,12),(18,12,112,12.00000,12.00000,'2014-05-14 14:25:47','ASD',1,12),(19,12,112,12.00000,12.00000,'2014-05-14 14:26:29','ASD',1,12),(20,12,112,12.00000,12.00000,'2014-05-14 14:30:41','ASD',1,12),(21,12,112,12.00000,12.00000,'2014-05-14 14:31:47','ASD',1,12),(22,9,9,9.00000,9.00000,'2014-05-14 14:34:36','hokl',1,9),(23,9,9,9.00000,9.00000,'2014-05-14 14:34:46','hokl',1,9),(24,9,9,9.00000,9.00000,'2014-05-14 14:35:14','hokl',1,9),(25,1,3,4.00000,5.00000,'2014-05-14 14:36:57','pro1',1,2),(26,2,4,5.00000,6.00000,'2014-05-14 14:37:06','pro2',1,3),(27,155,15,15.00000,15.00000,'2014-05-16 09:27:33','asdasdasd2222',1,1),(28,15,15,15.00000,15.00000,'2014-05-16 09:32:41','bondiola bondiolera justiciera',1,15);
 /*!40000 ALTER TABLE `producto` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -123,7 +151,7 @@ CREATE TABLE `carrito` (
 
 LOCK TABLES `carrito` WRITE;
 /*!40000 ALTER TABLE `carrito` DISABLE KEYS */;
-INSERT INTO `carrito` VALUES (0,0,20.00000,'0000-00-00 00:00:00'),(1,0,30.00000,'0000-00-00 00:00:00'),(2,1,30.00000,'0000-00-00 00:00:00');
+INSERT INTO `carrito` VALUES (0,0,20.00000,'0000-00-00 00:00:00'),(1,0,30.00000,'0000-00-00 00:00:00'),(2,1,30.00000,'0000-00-00 00:00:00'),(3,7,NULL,'2014-05-13 09:42:56'),(4,7,NULL,'2014-05-13 10:02:42'),(5,7,NULL,'2014-05-13 10:03:06'),(6,7,NULL,'2014-05-13 10:03:30'),(7,7,NULL,'2014-05-13 10:07:36'),(8,7,NULL,'2014-05-13 10:11:59'),(9,7,NULL,'2014-05-13 10:13:23'),(10,7,NULL,'2014-05-13 12:57:55');
 /*!40000 ALTER TABLE `carrito` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -144,7 +172,7 @@ CREATE TABLE `caja` (
   `datetime` datetime DEFAULT NULL,
   `habilitado` tinyint(1) DEFAULT '1',
   PRIMARY KEY (`idcaja`)
-) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=26 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -153,8 +181,79 @@ CREATE TABLE `caja` (
 
 LOCK TABLES `caja` WRITE;
 /*!40000 ALTER TABLE `caja` DISABLE KEYS */;
-INSERT INTO `caja` VALUES (1,'2014-04-25 12:59:19',800.45801,'2014-04-25 13:13:12',900.59497,2,'2014-04-25 13:13:12',1),(2,'2014-04-25 13:17:15',800.89001,'2014-04-25 14:01:13',3990.85596,3,'2014-04-25 14:01:13',1),(3,'2014-04-25 13:54:17',880.50000,'2014-04-25 13:55:13',970.59497,3,'2014-04-25 13:55:13',1),(4,'2014-04-25 13:54:19',880.50000,'2014-04-25 13:55:42',970.59497,3,'2014-04-25 13:55:42',1),(5,'2014-04-25 13:58:38',800.90002,'2014-04-25 14:00:53',9990.85645,3,'2014-04-25 14:00:53',1),(6,'2014-04-25 13:59:21',800.90002,NULL,NULL,3,'2014-04-25 13:59:21',1);
+INSERT INTO `caja` VALUES (1,'2014-04-25 12:59:19',800.45801,'2014-04-25 13:13:12',900.59497,2,'2014-04-25 13:13:12',1),(2,'2014-04-25 13:17:15',800.89001,'2014-04-25 14:01:13',3990.85596,3,'2014-04-25 14:01:13',1),(3,'2014-04-25 13:54:17',880.50000,'2014-05-07 11:45:19',5.00000,3,'2014-05-07 11:45:19',1),(4,'2014-04-25 13:54:19',880.50000,'2014-04-25 13:55:42',970.59497,3,'2014-04-25 13:55:42',1),(5,'2014-04-25 13:58:38',800.90002,'2014-04-25 14:00:53',9990.85645,3,'2014-04-25 14:00:53',1),(6,'2014-04-25 13:59:21',800.90002,'2014-05-07 11:46:30',5.00000,3,'2014-05-07 11:46:30',1),(7,NULL,NULL,NULL,500.00000,NULL,NULL,1),(24,NULL,NULL,NULL,250.00000,NULL,NULL,1),(25,NULL,NULL,NULL,750.00000,NULL,NULL,1);
 /*!40000 ALTER TABLE `caja` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `perfil`
+--
+
+DROP TABLE IF EXISTS `perfil`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `perfil` (
+  `idperfil` int(11) NOT NULL AUTO_INCREMENT,
+  `detalle` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`idperfil`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `perfil`
+--
+
+LOCK TABLES `perfil` WRITE;
+/*!40000 ALTER TABLE `perfil` DISABLE KEYS */;
+/*!40000 ALTER TABLE `perfil` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `turno`
+--
+
+DROP TABLE IF EXISTS `turno`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `turno` (
+  `idturno` int(11) NOT NULL,
+  `descripcion` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`idturno`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `turno`
+--
+
+LOCK TABLES `turno` WRITE;
+/*!40000 ALTER TABLE `turno` DISABLE KEYS */;
+INSERT INTO `turno` VALUES (0,'turno uno'),(1,'turno dos'),(2,'turno tres');
+/*!40000 ALTER TABLE `turno` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `rubro`
+--
+
+DROP TABLE IF EXISTS `rubro`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `rubro` (
+  `idrubro` int(11) NOT NULL AUTO_INCREMENT,
+  `descripcion` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`idrubro`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `rubro`
+--
+
+LOCK TABLES `rubro` WRITE;
+/*!40000 ALTER TABLE `rubro` DISABLE KEYS */;
+INSERT INTO `rubro` VALUES (0,'Varios'),(1,'Bebidas'),(2,'Cafeteria'),(3,'Comedor'),(4,'Kiosco'),(5,'Pancho');
+/*!40000 ALTER TABLE `rubro` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -184,53 +283,6 @@ LOCK TABLES `stock_caja_diario` WRITE;
 UNLOCK TABLES;
 
 --
--- Table structure for table `turno`
---
-
-DROP TABLE IF EXISTS `turno`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `turno` (
-  `idturno` int(11) NOT NULL,
-  `descripcion` varchar(45) DEFAULT NULL,
-  PRIMARY KEY (`idturno`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `turno`
---
-
-LOCK TABLES `turno` WRITE;
-/*!40000 ALTER TABLE `turno` DISABLE KEYS */;
-/*!40000 ALTER TABLE `turno` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `rubro`
---
-
-DROP TABLE IF EXISTS `rubro`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `rubro` (
-  `idrubro` int(11) NOT NULL AUTO_INCREMENT,
-  `descripcion` varchar(45) DEFAULT NULL,
-  PRIMARY KEY (`idrubro`)
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `rubro`
---
-
-LOCK TABLES `rubro` WRITE;
-/*!40000 ALTER TABLE `rubro` DISABLE KEYS */;
-INSERT INTO `rubro` VALUES (0,'Varios'),(1,'Bebidas'),(2,'Cafeteria'),(3,'Comedor'),(4,'Kiosco'),(5,'Pancho');
-/*!40000 ALTER TABLE `rubro` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
 -- Table structure for table `egreso`
 --
 
@@ -245,7 +297,7 @@ CREATE TABLE `egreso` (
   `cantidad` int(11) DEFAULT NULL,
   `datetime` datetime DEFAULT NULL,
   PRIMARY KEY (`idegreso`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -254,7 +306,7 @@ CREATE TABLE `egreso` (
 
 LOCK TABLES `egreso` WRITE;
 /*!40000 ALTER TABLE `egreso` DISABLE KEYS */;
-INSERT INTO `egreso` VALUES (1,20.00000,1,0,5,'0000-00-00 00:00:00'),(2,10.00000,1,1,1,'0000-00-00 00:00:00'),(3,20.00000,0,0,5,'0000-00-00 00:00:00'),(4,10.00000,2,0,10,'0000-00-00 00:00:00');
+INSERT INTO `egreso` VALUES (1,20.00000,1,0,5,'0000-00-00 00:00:00'),(2,10.00000,1,1,1,'0000-00-00 00:00:00'),(3,20.00000,0,0,5,'0000-00-00 00:00:00'),(4,10.00000,2,0,10,'0000-00-00 00:00:00'),(5,NULL,3,12,3,'2014-05-13 09:38:03'),(6,NULL,3,13,15237,'2014-05-13 09:38:03'),(7,NULL,3,15,1111,'2014-05-13 09:42:56'),(8,NULL,3,16,2222,'2014-05-13 09:42:57'),(9,40.00000,5,0,22,'2014-05-13 10:05:58'),(10,NULL,7,16,11111,'2014-05-13 10:07:37'),(11,44444.00000,9,0,11111,'2014-05-13 10:13:23'),(12,44444.00000,10,0,11111,'2014-05-13 12:57:55');
 /*!40000 ALTER TABLE `egreso` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -286,10 +338,6 @@ LOCK TABLES `ingreso` WRITE;
 INSERT INTO `ingreso` VALUES (0,1,1,0,0,20,'0000-00-00 00:00:00'),(1,1,0,0,1,30,'0000-00-00 00:00:00'),(2,1,2,0,0,10,'0000-00-00 00:00:00');
 /*!40000 ALTER TABLE `ingreso` ENABLE KEYS */;
 UNLOCK TABLES;
-
---
--- Dumping events for database 'Kiosco'
---
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -300,4 +348,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2014-04-28 16:31:29
+-- Dump completed on 2014-05-16 14:09:29
